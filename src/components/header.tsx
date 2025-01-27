adfa3dfc3896416bc6980ec0944ffac41b0e7625
joeribreedveld <joeri.breedveld123@gmail.com>
2025-01-27 19:54:18 +0100

    feat: style and copy changes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -37,7 +37,7 @@ export default async function Header() {
               className="flex h-8 items-center justify-center rounded-md border border-[#242424] bg-[#0A0A0A] px-3 text-sm font-medium shadow-sm transition hover:border-[#2B2B2B] hover:bg-[#1F1F1F] hover:text-[#EDEDED]"
               href="/login"
             >
-              Login
+              Sign in
             </Link>
           )}
         </div>
