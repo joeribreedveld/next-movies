2e35c7f1b9f3880e391dec396ee5174f47ee6883
joeribreedveld <joeri.breedveld123@gmail.com>
2025-01-31 11:27:47 +0100

    fix: last fix for safari

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,10 +6,7 @@ export default async function Header() {
   const session = await auth();
 
   return (
-    <header
-      style={{ position: "-webkit-sticky" }}
-      className="sticky top-0 z-10 flex h-16 w-full items-center justify-between border-b border-[#232222] bg-black"
-    >
+    <header className="sticky top-0 z-10 flex h-16 w-full items-center justify-between border-b border-[#232222] bg-black">
       <div className="container mx-auto flex items-center justify-between px-4">
         <div>
           <Link href="/">
